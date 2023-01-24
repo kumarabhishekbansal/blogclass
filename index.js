11e6489d8deb6487e3451e3ec8593607f7efb57b
kumarabhishekbansal <kumarabhishekbansal>
2023-01-24 12:56:28 +0530

    db change

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use("/auth",authrouter);
 app.use("/blog",blogrouter);
 // connection with mongo db
 
-mongoose.connect("mongodb://localhost:27017/blogclass");
+mongoose.connect("mongodb+srv://Abhishek:foG2pCKaFlF0yShL@cluster0.76z2f9m.mongodb.net/blogclass?retryWrites=true&w=majority");
 mongoose.connection.on("connected",()=>{
     console.log("mongo db connect success");
 })
